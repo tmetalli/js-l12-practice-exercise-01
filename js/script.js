5b29b38806f4e7826f8b09622dc86d811f08b362
T Metalli <t6metalli@yahoo.com>
2022-11-29 22:05:58 +0000

    added dropdown

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
+const selectUserNumber = document.querySelector("#users");
 const randomFolks = document.querySelector(".random-peeps");
 
-const getData = async function () {
-    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     // console.log(data);
     const userResults = data.results;
     displayUsers(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
     randomFolks.innerHTML = "";
@@ -22,8 +23,11 @@ const displayUsers = function (userResults) {
         <p><${country}</p>
         <img src=${imageUrl} alt="User avatar" />`;
         randomFolks.append(userDiv);
-    }
-    
+    } 
 };
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
 
 
